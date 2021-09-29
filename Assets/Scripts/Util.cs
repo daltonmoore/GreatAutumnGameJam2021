f3f22e3e254161422a5903c34817538a5a043aa7
DaltonM <daltonraymoore@gmail.com>
2021-09-29 07:05:23 -0400

    Update Util.cs

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -8,6 +8,7 @@ public class Util : MonoBehaviour
     {
         GameObject audio = Instantiate(Resources.Load<GameObject>("Prefabs/AudioSource"));
         AudioSource audioSource = audio.GetComponent<AudioSource>();
+        audioSource.outputAudioMixerGroup = MusicManager.Instance.mixer.FindMatchingGroups("Master")[0];
         audioSource.clip = audioClip;
         audioSource.Play();
         Destroy(audio, audioClip.length);
